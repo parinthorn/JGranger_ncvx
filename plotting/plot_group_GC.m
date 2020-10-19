21eea5a1fce1d1fb37c7af95da7ede8769828fb9
parinthorn <parinthorn@gmail.com>
2020-10-20 00:01:52 +0700

    update formulation D estimation

--- plotting/plot_group_GC.m
+++ plotting/plot_group_GC.m
@@ -1,14 +1,19 @@
 function s = plot_group_GC(GC)
 % size(GC) = [n,n,K]
 [n,~,K] = size(GC);
-commonNZ = ones(n,n);
+commonNZ = ones(n,n)-eye(n);
+diag_ind = 1:n+1:n^2;
 for kk=1:K
-    commonNZ = commonNZ & (GC(:,:,kk)~=0);
+    tmp = GC(:,:,kk);
+    tmp(diag_ind) = 0;
+    commonNZ = commonNZ & (tmp~=0);
 end
 s = figure;
 for kk =1:K
+        tmp = GC(:,:,kk);
+    tmp(diag_ind) = 0;
     subplot(1,K,kk)
-    spy(GC(:,:,kk),'r')
+    spy(tmp,'r')
     hold on
     spy(commonNZ,'k')
     hold off
