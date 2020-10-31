c87badfef4633fb6757dbdd072beb43fee4aa4e0
parinthorn <parinthorn@gmail.com>
2020-10-31 16:20:12 +0700

    update test code

--- experiment/Experiment_compare/proposed_method/experiment_D.m
+++ experiment/Experiment_compare/proposed_method/experiment_D.m
@@ -5,22 +5,26 @@ inpath = './data_compare/';
 outpath = '../formulation_D_result/';
 
 type = 2; %D type
-cd = 3; %common density set to percent(cd); percent=[1%, 5%, 10%, 20%]
+% cd = 3; %common density set to percent(cd); percent=[1%, 5%, 10%, 20%]
+cd = 2;
 T = 100;
-p = 3;
+p_true = 1;
+p_est = 1;
 K = 5;
 n = 20; % time-series channels
-[P,~] = offdiagJSS(n,p,K);
-load([inpath,'model_K',int2str(K),'_p1']) % struct E
+[P,~] = offdiagJSS(n,p_est,K);
+% load([inpath,'model_K',int2str(K),'_p',int2str(p_true)]) % struct E
+load([inpath,'test_model_K',int2str(K),'_p',int2str(p_true)]) % struct E
 [~,~,dd,m] = size(E);
+realz = 5;
 GridSize = 30;
 mname = {'1','5'};
 for ii=1:dd
-    for jj=1:m
+    for jj=1:realz
         % generate data from given seed
         model = E{type,cd,ii,jj};
         y = sim_VAR(model.A,T,1,model.seed,0);
-        M = formulation_D(y,P,p,GridSize);
+        M = formulation_D(y,P,p_est,GridSize);
 %         save([outpath,'result_formulationD_',mname{ii},'percent','_',int2str(jj),'_p',int2str(p)],'M')
     end
 end
