e40d1a6aebd2f422169aa337620894acb1429266
parinthorn <parinthorn@gmail.com>
2020-10-31 22:13:39 +0700

    upload problem demo

--- experiment/Experiment_compare/proposed_method/visualize_result.m
+++ experiment/Experiment_compare/proposed_method/visualize_result.m
@@ -1,11 +1,18 @@
 close all
 clc
-cd = 3;
+cd = 2;
 
-realz = 7;
+realz = 1;
 type = 2; %D
 mname = {'1','5'};
 dd=1;
+tmp=[model_acc(dd,realz).M.model];
+tmp = [tmp.stat];
+tmp = [tmp.model_selection_score];
+tmp = reshape([tmp.bic],[30,30]);
+figure(99);
+imagesc(tmp)
+
 true_model = E{type,cd,dd,realz};
 plot_group_GC(true_model.GC);
 set(figure(1),'Position',[962 819 720 176])
@@ -13,11 +20,12 @@ sgtitle(['true diff density:',mname{dd}])
 
 
 ind = model_acc(dd,realz).M.index.bic;
+% ind = 342;
 estimated_model = model_acc(dd,realz).M.model(ind);
 plot_group_GC(estimated_model.GC);
 set(figure(2),'Position',[963 585 719 148])
 sgtitle(['est diff density:',mname{dd}])
-model_acc(dd,realz).result(ind).differential
+model_acc(dd,realz).result(ind).total
 
 dd = 2;
 true_model = E{type,cd,dd,realz};
@@ -27,12 +35,37 @@ set(figure(3),'Position',[962 819-500 720 176])
 sgtitle(['true diff density:',mname{dd}])
 
 
-ind = model_acc(dd,realz).M.index.bic;
 estimated_model = model_acc(dd,realz).M.model(ind);
 plot_group_GC(estimated_model.GC);
 set(figure(4),'Position',[963 585-500 719 148])
 sgtitle(['est diff density:',mname{dd}])
-model_acc(dd,realz).result(ind).differential
+model_acc(dd,realz).result(ind).total
+
+tmp=[model_acc(dd,realz).M.model];
+tmp = [tmp.stat];
+tmp = [tmp.model_selection_score];
+tmp = reshape([tmp.bic],[30,30]);
+figure;
+imagesc(tmp)
+
+%% ROC
+figure;
+subplot(1,2,1)
+dd=1;
+tmp = [model_acc(dd,realz).result.differential];
+FPR = reshape([tmp.FPR],[30,30]);
+TPR = reshape([tmp.TPR],[30,30]);
+plot(FPR,TPR)
+axis([0 1 0 1])
+axis('square')
+dd=2;
+tmp = [model_acc(dd,realz).result.differential];
+FPR = reshape([tmp.FPR],[30,30]);
+TPR = reshape([tmp.TPR],[30,30]);
+subplot(1,2,2)
+plot(FPR,TPR)
+axis([0 1 0 1])
+axis('square')
 %%
 
 close all
--- function/compare_sparsity.m
+++ function/compare_sparsity.m
@@ -12,7 +12,7 @@ switch toggle
                 TP(ii,jj) = TP(ii,jj)+length(intersect(ind_true,ind{ii,jj}));
                 FN(ii,jj) = FN(ii,jj)+length(setdiff(ind_true,ind{ii,jj}));
                 FP(ii,jj) = FP(ii,jj)+length(setdiff(ind{ii,jj},ind_true));
-                TN(ii,jj) = n^2-TP(ii,jj)-FN(ii,jj)-FP(ii,jj);
+                TN(ii,jj) = n^2-n-TP(ii,jj)-FN(ii,jj)-FP(ii,jj);
                 %                 end
             end
         end
@@ -28,32 +28,27 @@ switch toggle
                     TP(ii,jj) = TP(ii,jj)+length(intersect(ind_true{kk},ind{ii,jj}{kk}));
                     FN(ii,jj) = FN(ii,jj)+length(setdiff(ind_true{kk},ind{ii,jj}{kk}));
                     FP(ii,jj) = FP(ii,jj)+length(setdiff(ind{ii,jj}{kk},ind_true{kk}));
-                    TN(ii,jj) = n^2*K-TP(ii,jj)-FN(ii,jj)-FP(ii,jj);
                 end
+                TN(ii,jj) = n^2-n-TP(ii,jj)-FN(ii,jj)-FP(ii,jj);
             end
         end
     case 'single_common'
-        TP = 0;
-        FN = 0;
-        FP = 0;
-        TN = 0;
-        for kk=1:K
-            TP = TP+length(intersect(ind_true,ind));
-            FN = FN+length(setdiff(ind_true,ind));
-            FP = FP+length(setdiff(ind,ind_true));
-            TN = TN+n^2*K-TP-FN-FP;
-        end
+
+            TP = length(intersect(ind_true,ind));
+            FN = length(setdiff(ind_true,ind));
+            FP = length(setdiff(ind,ind_true));
+            TN = n^2-n-TP-FN-FP;
     case 'single_differential'
         TP = 0;
         FN = 0;
         FP = 0;
-        TN = 0;
         for kk=1:K
             TP = TP+length(intersect(ind_true{kk},ind{kk}));
             FN = FN+length(setdiff(ind_true{kk},ind{kk}));
             FP = FP+length(setdiff(ind{kk},ind_true{kk}));
-            TN = TN+n^2*K-TP-FN-FP;
+            
         end
+        TN = K*(n^2-n)-TP-FN-FP;
 end
 stat(:,:,1) = TP;
 stat(:,:,2) = TN;
