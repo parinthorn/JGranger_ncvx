b3d8c5e5595215a5c148af8cd0a96a1e08782fc9
parinthorn <parinthorn@gmail.com>
2020-11-10 09:24:32 +0700

    update formulation S eval code

---
+++ function/model_selection_S.m
@@ -0,0 +1,27 @@
+function score = model_selection_S(Y,A,df)
+
+[n,~,p,K] = size(A);
+fitting = log_likelihood_var(Y,A,n,p,K);
+Num = size(Y,2);
+score.bic = fitting + log(Num)*df;
+score.aic = fitting + 2*df;
+score.aicc = score.aic + (2*df^2+2*df)/(Num-df-1);
+score.L = fitting;
+score.df = df;
+end
+function LLH = log_likelihood_var(data,A,n,p,K)
+Num = size(data,2);
+LLH = 0;
+tmpA = reshape(A,[n,n*p,K]);
+for kk=1:K
+    [H,Y] = H_gen(data(:,:,kk),p);
+    Ek = Y - tmpA(:,:,kk)*H; % Error term
+    Sigma = Ek*Ek'/(Num);
+    Sigma = (Sigma+Sigma')/2;
+    L = chol(Sigma,'lower');
+    logdetSigma = 2*sum(log(diag(L)));
+%     disp(size(Sigma))
+    LLH = LLH+(Num)*logdetSigma;%+(-1/2)*trace(Ek'*(Sigma\eye(n))*Ek);
+end
+
+end
