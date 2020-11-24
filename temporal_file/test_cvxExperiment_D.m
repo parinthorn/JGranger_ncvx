a815f6ebf4ed6bc03244c77a4705f3bb550effaf
parinthorn <parinthorn@gmail.com>
2020-11-24 18:13:02 +0700

    add cvx test

---
+++ temporal_file/test_cvxExperiment_D.m
@@ -0,0 +1,31 @@
+%% This experiment estimate VAR with formulation D by ADMM
+clear
+clc
+inpath = './data_compare/';
+outpath = 'G:/My Drive/0FROM_SHARED_DRIVE/THESIS/formulation_D_result/';
+mkdir(outpath)
+
+type = 2; %D type
+% cd = 3; %common density set to percent(cd); percent=[1%, 5%, 10%, 20%]
+cd = 3;
+T = 100;
+p_true = 1;
+p_est = 1;
+K = 50;
+n = 20; % time-series channels
+[P,~] = offdiagJSS(n,p_est,K);
+load([inpath,'model_K',int2str(K),'_p',int2str(p_true)]) % struct E
+[~,~,dd,m] = size(E);
+realz = 20;
+GridSize = 30;
+mname = {'1','5'};
+for ii=1:dd
+    for jj=1:realz
+        % generate data from given seed
+        model = E{type,cd,ii,jj};
+        y = sim_VAR(model.A,T,1,model.seed,0);
+        M = test_cvxformulation_D(y,P,p_est,GridSize);
+        save([outpath,'cvx_result_formulationD_',mname{ii},'percent','_lag',int2str(p_est),'_K',int2str(K),'_',int2str(jj)],'M')
+%     result_formulationD_1percent_lag1_K5_12
+    end
+end
--- temporal_file/test_group_lasso_df.m
+++ temporal_file/test_group_lasso_df.m
@@ -5,36 +5,37 @@ clear
 clc
 load('D:\JGranger_ncvx\data_compare\model_K50_p1.mat') % gt model
 realization=3;
-GTmodel = E{2,3,1,realization};
+GTmodel = E{2,3,2,realization};
 n=20;p=1;K=50;
 T = 100;
-load(['G:\My Drive\0FROM_SHARED_DRIVE\THESIS\formulation_D_result\result_formulationD_1percent_lag1_K50_',int2str(realization),'.mat'])
+load(['G:\My Drive\0FROM_SHARED_DRIVE\THESIS\formulation_D_result\result_formulationD_5percent_lag1_K50_',int2str(realization),'.mat'])
 GridSize = M.GridSize;
-y = sim_VAR(GTmodel.A,T,1,GTmodel.seed,0);
-H = zeros(n*p,T-p,K);
-Y = zeros(n,T-p,K);
-disp('Generating H matrix')
-for kk=1:K
-    [H(:,:,kk),Y(:,:,kk)] = H_gen(y(:,:,kk),p);
-end
+% y = sim_VAR(GTmodel.A,T,1,GTmodel.seed,0);
+% H = zeros(n*p,T-p,K);
+% Y = zeros(n,T-p,K);
+% disp('Generating H matrix')
+% for kk=1:K
+%     [H(:,:,kk),Y(:,:,kk)] = H_gen(y(:,:,kk),p);
+% end
 
 
 %%
 for ii=1:GridSize
     for jj=1:GridSize
         model = M.model(ii,jj);
-        
+        BLOCK_SIZE = p*K;
         idx = efficient_vect([n,p,K]);
         x_reg = model.A_reg(idx);
         x_cls = model.A(idx);
-        tmp_reg = reshape(x_reg,[p*K,n^2]);
-        tmp_ls = reshape(x_cls,[p*K,n^2]);
+        tmp_reg = reshape(x_reg,[BLOCK_SIZE,n^2*K/BLOCK_SIZE]);
+        tmp_ls = reshape(x_cls,[BLOCK_SIZE,n^2*K/BLOCK_SIZE]);
         tmp_ls =sqrt(sum(tmp_ls.^2,1));
         tmp_reg = sqrt(sum(tmp_reg.^2,1));
-        df(ii,jj) = length(find(tmp_reg))+(p*K-1)*sum(tmp_reg./tmp_ls,'omitnan');
+        df(ii,jj) = length(find(tmp_reg))+(BLOCK_SIZE-1)*sum(tmp_reg./tmp_ls,'omitnan');
         df_lasso(ii,jj) = length(find(x_reg));
         bic(ii,jj) = log(T-p)*df(ii,jj)+model.stat.model_selection_score.L;
         bic_lasso(ii,jj) = model.stat.model_selection_score.bic;
+%         disp(bic_lasso(ii,jj)-log(T-p)*df_lasso(ii,jj)+log(T-p)*df(ii,jj)-bic(ii,jj))
     end
 end
 %
