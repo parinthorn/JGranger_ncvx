24bbee28bbb3473e1d1713838e7aaf3a6635cd65
parinthorn <parinthorn@gmail.com>
2020-11-24 18:14:49 +0700

    update code

--- experiment/Experiment_compare/proposed_method/experiment_D.m
+++ experiment/Experiment_compare/proposed_method/experiment_D.m
@@ -16,16 +16,16 @@ n = 20; % time-series channels
 [P,~] = offdiagJSS(n,p_est,K);
 load([inpath,'model_K',int2str(K),'_p',int2str(p_true)]) % struct E
 [~,~,dd,m] = size(E);
-realz = 5;
+realz = m;
 GridSize = 30;
 mname = {'1','5'};
 for ii=1:dd
-    for jj=1:realz
+    for jj=6:realz
         % generate data from given seed
         model = E{type,cd,ii,jj};
         y = sim_VAR(model.A,T,1,model.seed,0);
         M = formulation_D(y,P,p_est,GridSize);
-        save([outpath,'result_formulationD_',mname{ii},'percent','_lag',int2str(p_est),'_K',int2str(K),'_',int2str(jj)],'M')
+        save([outpath,'regrid_result_formulationD_',mname{ii},'percent','_lag',int2str(p_est),'_K',int2str(K),'_',int2str(jj)],'M')
 %     result_formulationD_1percent_lag1_K5_12
     end
 end
