8cf24560e3ed4ba2177d45dac6f39fdf66391cbc
parinthorn <parinthorn@gmail.com>
2020-11-29 09:40:23 +0700

    perf eval update

--- experiment/Experiment_compare/proposed_method/experiment_D_performance_eval.m
+++ experiment/Experiment_compare/proposed_method/experiment_D_performance_eval.m
@@ -4,12 +4,14 @@ inpath = './data_compare/';
 
 resultpath = 'G:\My Drive\0FROM_SHARED_DRIVE\THESIS\formulation_D_result\';
 % resultpath = 'D:\parinthorn_thesis\formulation_D_result\';
+load('G:\My Drive\0FROM_SHARED_DRIVE\THESIS\formulation_D_result\best_index_K50.mat','best_index')
 mname = {'1','5'};
 dd = length(mname);
 dd=1;
 % dd=2;
 realization = 20;
 load([inpath,'model_K50_p1'])
+
 for ii=1:dd
     for jj=1:realization
         fprintf('(%d,%d)\n',ii,jj)
@@ -18,7 +20,8 @@ for ii=1:dd
         load(fname)
         model_acc = performance_eval(M,GTmodel);
         toggle_list = {'total','common','differential'};
-        M.index.bic=M.index.bic;
+        M.index.bic=best_index(jj);
+        ALL_RESULT(ii,jj).model_acc = model_acc;
         for tt = 1:length(toggle_list)
             toggle = toggle_list{tt};
         R.(toggle).F1(ii,jj) =model_acc(M.index.bic).(toggle).F1;
@@ -30,4 +33,4 @@ for ii=1:dd
         
     end
 end
-save([resultpath,'formulation_D_result_regrid_K50','all'],'R')
+save([resultpath,'optimal_formulation_D_result_regrid_K50','all'],'R')
