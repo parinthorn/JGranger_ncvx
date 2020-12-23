1e23508bc0610d277f3039fa5234e6a87fabf63f
parinthorn <parinthorn@gmail.com>
2020-12-23 12:39:42 +0700

    test

--- temporal_file/test_varycd.m
+++ temporal_file/test_varycd.m
@@ -33,3 +33,40 @@ for ii=1:dd
     end
 end
 % end
+
+%% This experiment estimate VAR with formulation S by ADMM
+clear
+clc
+inpath = './data_compare/';
+outpath = 'G:/My Drive/0FROM_SHARED_DRIVE/THESIS/formulation_D_result/';
+mkdir(outpath)
+
+type = 2; %D type
+T = 100;
+p_true = 1;
+p_est = 1;
+K = 5;
+n = 20; % time-series channels
+[P,~] = offdiagJSS(n,p_est,K);
+Dtmp = diffmat(n,p_est,K);
+D = sparse(Dtmp*P);
+load([inpath,'vary_CD_model',int2str(K),'_p',int2str(p_true)]) % struct E
+[~,cc,dd,m] = size(E);
+realz = m;
+GridSize = 30;
+cname = {'2','4','5','6','8'};
+dname = {'8','6','5','4','2'};
+
+% cname = {'5','15','25'};
+% dname = {'25','15','5'};
+% for cd=1:cc
+for ii=1:5
+    for jj=1:realz
+        % generate data from given seed
+        model = E{type,ii,ii,jj};
+        y = sim_VAR(model.A,T,1,model.seed,0);
+        M = test_cvxformulation_S(y,P,D,p_est,GridSize);
+        save([outpath,'cvx_CDvary_result_formulationS_c',cname{ii},'d',dname{ii},'percent','_lag',int2str(p_est),'_K',int2str(K),'_',int2str(jj)],'M')
+%     result_formulationD_1percent_lag1_K5_12
+    end
+end
