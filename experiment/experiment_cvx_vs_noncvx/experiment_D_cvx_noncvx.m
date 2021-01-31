2227ac74192e5d928493bff152f70de95c87f777
parinthorn <parinthorn@gmail.com>
2021-02-01 00:00:01 +0700

    upload experiment compare cvx

---
+++ experiment/experiment_cvx_vs_noncvx/experiment_D_cvx_noncvx.m
@@ -0,0 +1,28 @@
+%% This experiment estimate VAR with formulation D by ADMM
+clear
+clc
+inpath = './data_compare/';
+% outpath = '../formulation_D_result/';
+outpath = 'G:\My Drive\0FROM_SHARED_DRIVE\THESIS\experiment_compare_cvx\';
+mkdir(outpath)
+
+T = 150;
+p_true = 3;
+p_est = 3;
+K = 5;
+load([inpath,'compare_convex_model_K',int2str(K),'_p',int2str(p_true)]) % struct E
+[~,~,dd,m] = size(E);
+realz = m;
+GridSize = 30;
+mname = {'1','5'};
+for jj=1:realz
+    % generate data from given seed
+    model = E{2,jj}; % type D
+    y = sim_VAR(model.A,T,1,model.seed,0);
+    M_ncvx = formulation_D(y,p_est,GridSize);
+    
+    save([outpath,'resultT150_adaptive_formulationD_',mname{ii},'percent','_lag',int2str(p_est),'_K',int2str(K),'_',int2str(jj)],'M')
+    M_cvx = test_cvxformulation_D(y,p_est,GridSize);
+    save([outpath,'resultT150_adaptive_formulationD_',mname{ii},'percent','_lag',int2str(p_est),'_K',int2str(K),'_',int2str(jj)],'M')
+
+end
