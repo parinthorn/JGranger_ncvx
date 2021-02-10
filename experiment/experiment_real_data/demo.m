40c83e81deaee24122a30d48c3fd1a2fd2783e03
parinthorn <parinthorn@gmail.com>
2021-02-10 22:18:57 +0700

    clear cache

--- experiment/experiment_real_data/demo.m
+++ experiment/experiment_real_data/demo.m
@@ -6,7 +6,9 @@ clc
 selected_TDC = {'0010023';'0010024';'0010070';'0010088';'0010092';'0010112';'0010122';'0010123';'0010128';'1000804';'1435954';'3163200';'3243657';'3845761';'4079254';'8692452';'8834383';'9750701'};
 selected_ADHD_C = {'0010013';'0010017';'0010019';'0010022';'0010025';'0010037';'0010042';'0010048';'0010050';'0010086';'0010109';'1187766';'1283494';'1918630';'1992284';'2054438';'2107638';'2497695'};
 y_TDC = concat_real_data(selected_TDC,116,'nyu');
+y_TDC = y_TDC-mean(y_TDC,2);
 y_ADHD_C = concat_real_data(selected_ADHD_C,116,'nyu');
+y_ADHD_C = y_ADHD_C-mean(y_ADHD_C,2);
 K = size(y_TDC,3);
 y_TDC_concat = reshape(y_TDC,[116,172*18]);
 y_ADHD_C_concat = reshape(y_ADHD_C,[116,172*18]);
@@ -17,6 +19,8 @@ y_total(:,:,2) = y_ADHD_C_concat;
 %% ESTIMATE MODEL USING CONCATENATION K=2
 M = test_cvxformulation_D(y_total,1,30);
 % save('G:\My Drive\0FROM_SHARED_DRIVE\THESIS\Real_data\experiment_real_data_result\estim_2K_D','M')
+M = test_cvxformulation_S(y_total,1,30);
+% save('G:\My Drive\0FROM_SHARED_DRIVE\THESIS\Real_data\experiment_real_data_result\estim_2K_S','M')
 %% ESTIMATE MODEL USING K = 18
 % FORMULATION C
 clear M
